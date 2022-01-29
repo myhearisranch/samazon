999fd382a9150addb8fa89dd579481737b2a7ef6
myhearisranch <myhearisranch@gmail.com>
2022-01-29 09:10:17 +0000

    04 create products table

--- db/migrate/20220129045208_create_products.rb
+++ db/migrate/20220129045208_create_products.rb
@@ -3,6 +3,11 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
 
       t.timestamps
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.references :category, foreign_key: true
+
     end
   end
 end
