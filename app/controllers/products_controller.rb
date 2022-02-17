5ec448f077e9b5e51222ee6b8f38e640ffcc5111
myhearisranch <myhearisranch@gmail.com>
2022-02-17 02:01:00 +0000

    カテゴリ検索機能 追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,39 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy, :favorite]
 
   def index
-    @products = Product.all
+
+      #product.rbで、display_listというメソッドを定義した。
+      # scope :display_list, -> (category, page) {
+      #     if category != "none"
+      #       where(category_id: category).page(page).per(PER)
+      #     else
+      #       page(page).per(PER)
+      #     end
+      #   }
+
+      @products = Product.display_list(category_params, params[:page])
+
+      #Categoryモデルに定義した
+      #category_paramsによって、送られたcategoryの値で識別
+
+      # scope :request_category, -> (category) {
+      #   if category != "none"
+      #     find(category.to_i)
+      #   else
+      #     ""
+      #   end
+      # }
+
+      @category = Category.request_category(category_params)
+      @categories = Category.all
+
+      #major_categoriesメソッドは、Categoryモデルに↓のように定義した。
+      #scope :major_categories, -> { pluck(:major_category_name).uniq }
+      #すべてのカテゴリのデータの中からmajor_category_nameのカラムのみを取得しています。そしてさらにuniqメソッドを使い、重複するデータを削除しています。
+
+      @major_category_names = Category.major_categories
+    end
   end
 
   def show
@@ -36,6 +67,16 @@ class ProductsController < ApplicationController
     redirect_to products_url
   end
 
+  def favorite
+
+    #user.toggle_like:
+    #商品に対する「いいね」の状態を現在の状態から逆にする。
+
+    current_user.toggle_like!(@product)
+    redirect_to product_url @product
+
+  end
+
   private
 
     def set_product
@@ -44,6 +85,16 @@ class ProductsController < ApplicationController
 
     def product_params
       params.require(:product).permit(:name, :description, :price, :category_id)
+    end
+
+
+    #<%= link_to category.name, products_path(category: category.id) %>で、
+    #category=>category.idを送る。
+    #それを、category_paramsで識別
+
+    def category_params
+      params[:category].present? ? params[:category]
+                                  : "none"
     end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,18 @@ class Product < ApplicationRecord
 
   belongs_to :category
   has_many :reviews
+  acts_as_likeable
+
+  PER=15
+
+ #display_listというメソッドを使い、条件によって表示する商品を変更するようにしています。
+  scope :display_list, -> (category, page){
+    if category != "none"
+      where(category_id: category).page(page).per(PER)
+    else
+      page(page).per(PER)
+    end
+  }
 
   def reviews_new
     review.new
